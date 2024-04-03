8820410206fd00556380f3c2f755b468f288396b
Italo Castillo <italogitdoe@gmail.com>
2024-04-03 23:08:10 +0200

    add random

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import './style.css'
- 
-
+import "./style.css";
 
 const div = document.getElementById("app");
 
 const interval = setInterval(addTumblr, 2000);
 
-function addTumblr(){
+function addTumblr() {
+  if (tumblr_api_read) {
+    const img = document.createElement("img");
+    img.setAttribute(
+      "src",
+      tumblr_api_read["posts"][rand(20)]["reblogged_from_avatar_url_512"]
+    );
 
-
-  const img = document.createElement('img');
-  img.setAttribute('src', tumblr_api_read['posts'][0]['reblogged_from_avatar_url_512']);
-
-  div?.append(img);
+    div?.append(img);
+  }
 }
 
+const rand = (x: number) => Math.floor(Math.random() * x);
