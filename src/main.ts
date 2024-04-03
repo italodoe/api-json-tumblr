7ccfe3b077520b73eb430a61ac061b0121b3c5f6
Italo Castillo <italogitdoe@gmail.com>
2024-04-03 22:48:29 +0200

    add tumblr api

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-console.log('hey')
+ 
+
+
+const div = document.getElementById("app");
+
+const interval = setInterval(addTumblr, 2000);
+
+function addTumblr(){
+
+
+  const img = document.createElement('img');
+  img.setAttribute('src', tumblr_api_read['posts'][0]['reblogged_from_avatar_url_512']);
+
+  div?.append(img);
+}
 
